6292fbdb84e00ff307e3f1705ca45a14b868c5b9
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2022-11-13 21:05:54 -0300

    add Poppins font

--- App.tsx
+++ App.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { Routes } from "./src/routes";
+import { useFonts, Poppins_400Regular } from '@expo-google-fonts/poppins';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Routes />;
 }
